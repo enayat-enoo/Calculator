5c0e5bd749c7b3abd5a813b362d76fe487a31169
Md Enayat Ansari <84342481+enayat-enoo@users.noreply.github.com>
2024-06-11 20:28:49 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,6 +3,8 @@ let buttons=document.querySelectorAll('button');
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
         if(e.target.innerHTML== '='){
+            //Avoid the implementation of eval()
+            //function implement your own function
             string=eval(string);
             document.querySelector('input').value=string;
         }else{
